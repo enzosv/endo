4f4c2e5720d0a5adbda04d0c33c5f7cc564bd30f
Enzo Vergara <vergara.enzo@gmail.com>
2018-10-14 02:01:12 +0800

    gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,77 @@
 "use strict";
-var gulp = require('gulp'),
-    useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    ngAnnotate = require('gulp-ng-annotate'),
-    uglify = require('gulp-uglify'),
-    cssnano = require('gulp-cssnano'),
-    runSequence = require('run-sequence'),
-    del = require('del'),
-    htmlmin = require('gulp-htmlmin'),
-    zip = require('gulp-vinyl-zip'),
-    purify = require('gulp-purifycss');
 
-var packageName = 'endo';
+const packageName = 'endo';
 
-gulp.task('minify-assets', function () {
-    return gulp.src('src/*.html')
+const gulp = require('gulp');
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const ngAnnotate = require('gulp-ng-annotate');
+const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const zip = require('gulp-vinyl-zip');
+const purify = require('gulp-purifycss');
+
+function minifyAssets(cb) {
+    gulp.src('src/*.html')
         .pipe(useref())
         .pipe(gulpif('*.js', ngAnnotate()))
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', purify(['./src/html/*.html', './src/*.html'])))
         .pipe(gulpif('*.css', cssnano()))
         .pipe(gulp.dest(packageName));
-});
+    cb();
+}
 
-gulp.task('minify-html', function () {
-    return gulp.src('src/html/*.html')
+function minifyHTML(cb) {
+    gulp.src('src/html/*.html')
         .pipe(htmlmin({
             collapseWhitespace: false
         }))
-        .pipe(gulp.dest(packageName + '/html'));
-});
+        .pipe(gulp.dest(packageName + '/html'))
+    cb();
+}
 
-gulp.task('clean-folder', function () {
-    return del([
+function cleanFolder(cb) {
+    del([
         packageName
     ]);
-});
+    cb();
+}
 
-gulp.task('copy-manifest', function () {
-    return gulp.src('src/manifest.json')
-        .pipe(gulp.dest(packageName));
-});
-
-gulp.task('copy-font', function () {
-    return gulp.src('src/css/fonts/*')
-        .pipe(gulp.dest(packageName + '/fonts'));
-});
-
-gulp.task('clean-zip', function () {
-    return del([
+function cleanZip(cb) {
+    del([
         packageName + '.zip'
     ]);
-});
+    cb();
+}
 
-gulp.task('zip', function () {
-    return gulp.src(packageName + "/**/*")
+function copyManifest(cb) {
+    gulp.src('src/manifest.json')
+        .pipe(gulp.dest(packageName));
+    cb();
+}
+
+function copyFont(cb) {
+    gulp.src('src/css/fonts/*')
+        .pipe(gulp.dest(packageName + '/fonts'));
+    cb();
+}
+
+function pack(cb) {
+    gulp.src(packageName + "/**/*")
         .pipe(zip.dest(packageName + '.zip'));
-});
+    cb();
+}
 
-gulp.task('default', function (callback) {
-    runSequence(['minify-assets', 'minify-html'],
-        callback);
-});
+const build = gulp.series(
+    cleanFolder,
+    gulp.parallel(minifyAssets, minifyHTML, copyManifest, copyFont)
+);
 
-gulp.task('recreate', function (callback) {
-    runSequence(['clean-folder', 'clean-zip'], ['minify-assets', 'minify-html', 'copy-manifest', 'copy-font'], 'zip',
-        callback);
-});
+exports.default = build
+
+exports.recreate = gulp.series(
+    gulp.parallel(build, cleanZip),
+    pack);
